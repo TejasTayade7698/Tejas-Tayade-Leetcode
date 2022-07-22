d50746111baabf49e560d37df8f827404d2d5e73
Tejas Tayade <92956577+TejasTayade7698@users.noreply.github.com>
2022-07-22 14:11:51 -0400

    Time: 105 ms (33.83%), Space: 13.7 MB (89.67%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res=max(nums)
+        curMax,curMin=1,1
+        for n in nums:
+            tmp=curMax*n
+            curMax=max(n*curMax, n*curMin, n)
+            curMin=min(tmp,n*curMin,n)
+            res=max(res,curMax)
+        return res
+    
