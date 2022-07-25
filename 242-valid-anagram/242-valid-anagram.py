e6050ae8ca87b27b3ee78f771e2c12ebb89eb5b1
Tejas Tayade <92956577+TejasTayade7698@users.noreply.github.com>
2022-07-25 08:03:16 -0400

    Time: 102 ms (13.43%), Space: 13.8 MB (89.78%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def isAnagram(self, s, t):
+        
+        
         cnt1 = Counter(s)
         cnt2 = Counter(t)
         return cnt1 == cnt2
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+       
+    
+        
+        
+        
+        
+        
         # i=(0,range(len(s)))
         # while i in range(len(s)):
         #     if s.count(s[i])==t.count(s[i]):
