7775c8a560359083f44e2a36c90409d7b499f3c9
Tejas Tayade <92956577+TejasTayade7698@users.noreply.github.com>
2022-07-25 09:51:14 -0400

    Time: 28 ms (99.09%), Space: 13.7 MB (59.37%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """              
+        dic={}
+        for i in ransomNote:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=0
+        
+        
+        for i in dic:
+            if magazine.count(i)<=dic[i]:
+                return False
+        return True
+            
+        
