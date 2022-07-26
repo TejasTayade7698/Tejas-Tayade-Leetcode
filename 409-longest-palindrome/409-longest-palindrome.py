73929da0594520191bc7c0cbe7742ef3a122edcf
Tejas Tayade <92956577+TejasTayade7698@users.noreply.github.com>
2022-07-26 02:35:31 -0400

    Time: 27 ms (72.16%), Space: 13.4 MB (64.21%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -4,5 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        odds = sum(v & 1 for v in collections.Counter(s).values())
-        return len(s) - odds + bool(odds)
+        #odds = sum(v & 1 for v in collections.Counter(s).values())
+        #return len(s) - odds + bool(odds)
+        
+        hash=set()
+        for i in s:
+            if i not in hash:
+                hash.add(i)
+            else:
+                hash.remove(i)
+        return len(s) - len(hash) + 1 if len(hash) > 0 else len(s)
+                
